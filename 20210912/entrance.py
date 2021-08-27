001742423689b4e7848588480ddb8ef704b41b90
driller <eleshis@gmail.com>
2021-08-27 16:54:00 +0900

    API -> GitHub

--- 20210912/entrance.py
+++ 20210912/entrance.py
@@ -1,30 +1,24 @@
 import asyncio
-import random
+import json
 from typing import Union
 
 import aiohttp
 
 
-async def get(symbol: str, start_time: int, end_time: int) -> Union[None, dict]:
-    url = "https://api.bybit.com/v2/public/liq-records"
+async def get() -> Union[None, dict]:
+    url = "https://raw.githubusercontent.com/fin-py/events/main/20210912/price.json"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(
             url,
-            params={"symbol": symbol, "start_time": start_time, "end_time": end_time},
         ) as response:
             if response.status == 200:
-                return await response.json()
+                return json.loads(await response.text())
 
 
-async def main(seed: int = 0) -> Union[None, float]:
-    if seed is None:
-        return
-
-    json_data = await get("BTCUSD", 1627743600, 1627743601)
+async def main(num: int = 0) -> Union[None, float]:
+    json_data = await get()
     if json_data:
-        random.seed(seed)
-        num = random.randrange(100)
         result = {x["id"]: x for x in json_data["result"]}
         id_ = sorted(result.keys())[num]
         price = int(result[id_]["price"])
