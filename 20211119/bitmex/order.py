8b7e480084ee9a32af7af98b600dbc43346b4e18
shinseitaro <shinsei.all@gmail.com>
2021-11-19 18:17:37 +0900

    rename file

--- 20211119/bitmex/order.py
+++ 20211119/bitmex/order.py
@@ -7,20 +7,23 @@ import pybotters
 
 logging.basicConfig(level=logging.DEBUG)
 
+base_url = "https://testnet.bitmex.com/api/v1"
+apis = "apis.json"
 
-async def order_create():
-    async with pybotters.Client(
-        base_url="https://testnet.bitmex.com/api/v1", apis="apis.json"
-    ) as client:
+
+async def order_create(
+    symbol, side, order_qty, price, order_type="Limit", time_in_force="GoodTillCancel"
+):
+    async with pybotters.Client(base_url=base_url, apis=apis) as client:
         r = await client.post(
             "/order",
             data={
-                "symbol": "XBTUSD",  # symbol だけが必須
-                "side": "Buy",  # or "Sell". Default Buy
-                "ordType": "Limit",
-                "orderQty": 100,
-                "price": 59400,
-                "timeInForce": "GoodTillCancel",
+                "symbol": symbol,
+                "side": side,
+                "orderQty": order_qty,
+                "price": price,
+                "ordType": order_type,
+                "timeInForce": time_in_force,
             },
         )
         data = await r.json()
@@ -28,5 +31,19 @@ async def order_create():
         return data
 
 
-if __name__ == "__main__":
-    asyncio.run(order_create())
+async def get_open_orders(symbol: str) -> dict:
+    async with pybotters.Client(base_url=base_url, apis=apis) as client:
+        r = await client.get("/order", data={"symbol": symbol})
+        data = await r.json()
+        return data
+
+
+async def order_cancel_all(symbol):
+    async with pybotters.Client(base_url=base_url, apis=apis) as client:
+        r = await client.delete("/order/all", data={"symbol": symbol})
+        data = await r.json()
+        return data
+
+
+# if __name__ == "__main__":
+#     asyncio.run(order_create())
